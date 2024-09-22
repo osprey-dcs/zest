fe4fa1b1e42018f8c3e506a111a776d42da33ac1
Larry Doolittle <LRDoolittle@lbl.gov>
2024-09-21 20:28:55 -0700

    Updates to QR code generation
    (1) Add non-zero quiet zone as default, so result is acually legible on my devices
    (2) Switch to error code Q and Alphanumeric input mode, so it fits in a Version 2 (25x25)
    (3) Minor whitespace tweaks
    Resulting QR code takes up same area on the silkscreen (29x29)

--- qrsn.py
+++ qrsn.py
@@ -1,6 +1,7 @@
 import pcbnew
 import pyqrcode
 
+
 def pcbtext(board,x,y,text,size=pcbnew.VECTOR2I(1000000,2000000),layer=None,hjustify=pcbnew.GR_TEXT_H_ALIGN_CENTER):
     c=pcbnew.PCB_TEXT(board)
     c.SetPos(pcbnew.VECTOR2I(int(round(x,10)),int(round(y,10))))
@@ -39,13 +40,21 @@ def pcbfilledrect(board,xys,layer='F.Silkscreen'):
     return c
 
 
-def qrgen(stringin,quiet_zone=0):
-    t=pyqrcode.create(stringin)
-    #t.svg('test.svg',scale=1)
+def qrgen(stringin,quiet_zone=2):
+    t=pyqrcode.create(stringin,error='Q')
+    # t.svg('test.svg',scale=1)
     text=[i for i in t.text(quiet_zone).split('\n') if i]
     return text
-def qrpcb(board,stringin,x0,y0,quiet_zone=0,stepnm=10*25400,layer='F.Silkscreen'):
+
+
+def qrpcb(board,stringin,x0,y0,quiet_zone=2,stepnm=10*25400,layer='F.Silkscreen'):
     text=qrgen(stringin,quiet_zone)
+    # print("QR size %d" % len(text))
+    # Checks that we got Version 2 (25x25) plus the quiet zone
+    # succeeds with Alphanumeric input mode and error correction level 'Q'
+    # Proper overall layout of the QR code in relation to the nearby text
+    # on the silkscreen depends on this size being (about) 29.
+    assert(len(text) == 29)
     cs=[]
     text.reverse()
     for il,l in enumerate(text):
@@ -69,28 +78,30 @@ if __name__=="__main__":
     import os
     sys.path.append('./scripts/')
     import gerber
-    
+
     parser = argparse.ArgumentParser(description=__doc__)
 #    parser.add_argument(dest='project', help='project name (used in saved gerber file name)')
     parser.add_argument('-start', dest='start',type=int, default=1,help='serial number start')
     parser.add_argument('-stop', dest='stop',type=int, default=0,help='serial number stop')
-    parser.add_argument('-quiet', dest='quiet',type=int, default=0,help='quiet zone')
-    parser.add_argument('-keeppcb', dest='keeppcb',action='store_true',help='keep the pcberory file')
+    parser.add_argument('-quiet', dest='quiet',type=int, default=2,help='quiet zone')
+    parser.add_argument('-keeppcb', dest='keeppcb',action='store_true',help='keep the pcb file')
     parser.add_argument('-projname', dest='projname',type=str,help='proj name',default='_qrsn')
     parser.add_argument('-outputdir', dest='outputdir',type=str,help='output directory',default='qrsngbr')
 
     clargs = parser.parse_args()
-    
+
     boardfilename='%s.kicad_pcb'%clargs.projname
     for sn in range(clargs.start,clargs.stop):
-        gitcommit8=git.Repo('.').head.commit.hexsha[0:8]
+        gitcommit8=git.Repo('.').head.commit.hexsha[0:8].upper()
         if os.path.isfile(boardfilename):
             board=pcbnew.LoadBoard(boardfilename)
         else:
             board=pcbnew.NewBoard(boardfilename)
 #        qrpcb(board=board,stringin='https://gitlab.com/lbl-boards/zest commit:%s SN:%03d'%(gitcommit8,sn),x0=80000000,y0=-158000000,stepnm=15*25400,layer='B.Silkscreen')
-        qrpcb(board=board,stringin='commit:%s SN:%03d'%(gitcommit8,sn),x0=90000000,y0=-203000000,stepnm=13*25400,layer='F.Silkscreen',quiet_zone=clargs.quiet)
-        pcbtext(board=board,x=101000000,y=-211000000,text="Commit:\n%s\nSN:"%(gitcommit8),size=pcbnew.VECTOR2I(1500000,1500000),layer='F.Silkscreen',hjustify=pcbnew.GR_TEXT_H_ALIGN_LEFT)
+        stringin = ('commit:%s SN:%03d'%(gitcommit8,sn)).upper()
+        print(stringin)
+        qrpcb(board=board,stringin=stringin,x0=90000000,y0=-203000000,stepnm=13*25400,layer='F.Silkscreen',quiet_zone=clargs.quiet)
+        pcbtext(board=board,x=101000000,y=-211000000,text="COMMIT:\n%s\nSN:"%(gitcommit8),size=pcbnew.VECTOR2I(1500000,1500000),layer='F.Silkscreen',hjustify=pcbnew.GR_TEXT_H_ALIGN_LEFT)
         pcbtext(board=board,x=101000000,y=-204000000,text="%03d"%(sn),size=pcbnew.VECTOR2I(5000000,5000000),layer='F.Silkscreen',hjustify=pcbnew.GR_TEXT_H_ALIGN_LEFT)
         pcbnew.Refresh()
         board.Save(boardfilename)
@@ -101,5 +112,3 @@ if __name__=="__main__":
         if not clargs.keeppcb:
             for ext in ['pcb','pro']:
                 os.remove('%s.kicad_%s'%(clargs.projname,ext))
-    
-
